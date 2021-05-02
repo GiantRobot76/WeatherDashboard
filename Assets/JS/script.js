b9b872f1651446b23c508e1c296d7d95119eca6e
Jon Bowman <jon.m.bowman@gmail.com>
2021-05-02 12:22:10 -0600

    Dynamically Added Items Now Clickable

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,5 +1,7 @@
 var searchButton = $("#search-button");
 var cityInput = $("#city-input");
+var cityList = $("#city-list");
+var svdList = $(".list-item");
 var currentCity;
 
 var requestUrl;
@@ -23,13 +25,11 @@ function getCurrentWeatherAPI() {
     .then(function (data) {
       if (data.cod == "404") {
         cityName.text("City Not Found");
+
         return;
       }
       cityName.text(data.name);
-      console.log(data);
-
-      if (data.cod == "404") {
-      }
+      //   console.log(data);
 
       localTemperature.text(data.main.temp);
       localWind.text(data.wind.speed);
@@ -69,9 +69,36 @@ searchButton.on("click", function (event) {
 
   //get city in input field. If it is two words, replace the " " with a "+" as required by the API"
   currentCity = cityInput.val().replace(" ", "+").trim();
+
+  //keep original format for 2 name cities for display on appended li
+  var formatCity = currentCity.replace("+", " ");
+
+  //build request URL using base API string and input city name
   requestUrl =
     "https://api.openweathermap.org/data/2.5/weather?q=" +
     currentCity +
     "&units=imperial&appid=255055a794435e93d10c1986c06d9c9b";
   getCurrentWeatherAPI();
+
+  var appendLi = $("<button>")
+    .addClass("list-item")
+    .attr("id", currentCity)
+    .text(formatCity)
+    .on("click", function () {
+      currentCity = $(this).text();
+      requestUrl =
+        "https://api.openweathermap.org/data/2.5/weather?q=" +
+        currentCity +
+        "&units=imperial&appid=255055a794435e93d10c1986c06d9c9b";
+      getCurrentWeatherAPI();
+    });
+
+  cityList.append(appendLi);
+  cityInput.val("");
+});
+
+svdList.on("click", function (event) {
+  event.preventDefault();
+  console.log("list item pushed");
+  console.log($(this).attr("id"));
 });
